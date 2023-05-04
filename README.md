8ef4823b570ed6853673262d2b5ebdaf6ec2e6e7
Mark Wylde <129744061+markwylde-deriv@users.noreply.github.com>
2023-05-04 15:48:24 +0100

    Intial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Debug
+
+This is a developer section of deriv experiments, to explore and deep dive into the websocket/state of the Deriv experiments site.
+
+## Installation
+
+```
+git clone https://github.com/deriv-experiments/debug.git
+cd debug
+npm install
+npm run start
+```
+
+Then visit:
+
+http://localhost:8002
---
+++ src/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Deriv, { useAuthorize, useDebug } from '@deriv-experiments/react'
+
+const StatementPage = () => {
+  const authorize = useAuthorize();
+
+  const debug = useDebug();
+
+  return (
+    <div>
+      <h1>Debug</h1>
+
+      {authorize
+        ? (
+          <>
+            Logged in as {' '}
+            <strong>{authorize.email}</strong>
+          </> 
+        )
+        : (
+          <button onClick={Deriv.login}>Login</button>
+        )
+      }
+
+      <ul>
+      <li><a href="/">Dashboard</a></li>
+        <li><a href="/apps/">Apps</a></li>
+        <li><a href="/reports/">Reports</a></li>
+        <li>Debug</li>
+      </ul>
+
+      <h2>WebSocket Log</h2>
+      <ul>
+        {debug?.map((entry, index) => {
+          return (
+            <li key={index}>
+              <strong>
+                {entry[0]}
+              </strong>
+              <br />
+              <pre><code>{JSON.stringify(entry[1], null, 2)}</code></pre>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(
+  <StatementPage />
+);
